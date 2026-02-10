1acc8fa559a405030c39e0c63546671ce268f720
Ilsa Khurshid Qureshi <124763286+IlsaKQureshi@users.noreply.github.com>
2026-02-10 17:22:13 +0100

    added footer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 
 
 export default function App() {
-  return <div className="App">SheCodes Weather App </div>
+  return (
+
+    <div className="App">SheCodes Weather App
+      <footer>
+        This project was coded by Ilsa Khurshid Qureshi and is {" "}
+        <a href="https://github.com/IlsaKQureshi/react-weather-app-project" target="_blank">open-sourced</a>
+      </footer></div>)
 }
 
 
