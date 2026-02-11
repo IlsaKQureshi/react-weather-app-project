715afcb4726db46b5eea0b418a11630d414a9834
Ilsa Khurshid Qureshi <124763286+IlsaKQureshi@users.noreply.github.com>
2026-02-11 14:48:02 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import './App.css';
 export default function App() {
   return (
 
-    <div className="App">SheCodes Weather App
-      <footer>
-        This project was coded by Ilsa Khurshid Qureshi and is
-        <a href="https://github.com/IlsaKQureshi/react-weather-app-project" target="_blank" rel="noreferrer">open-sourced</a>
-      </footer></div>)
+    <div className="App">
+      <div className="container">
+        <h1>SheCodes Weather App</h1>
+        <footer>
+          This project was coded by Ilsa Khurshid Qureshi and is{" "}
+          <a href="https://github.com/IlsaKQureshi/react-weather-app-project" target="_blank" rel="noreferrer">open-sourced</a>
+        </footer></div></div>)
 }
 
 
